d45e5d6a11ea208701f436029540ea95315bf526
adam-james <adam.j.sulewski@gmail.com>
2015-12-03 11:39:26 -0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var webpack = require('webpack-stream');
+
+gulp.task('static:dev', function() {
+  return gulp.src('app/index.html')
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build:dev', function() {
+  return gulp.src('app/entry.js')
+    .pipe(webpack({
+      output: {
+        filename: 'bundle.js'
+      },
+      module: {
+        loaders: [
+          {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+              presets: ['react']
+            }
+          }
+        ]
+      }
+    }))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('default', ['static:dev', 'build:dev']);
